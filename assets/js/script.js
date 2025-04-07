61ce02376ddfc755f3cec7ad2f462160f96de044
saraelizalde <j7esg480@students.codeinstitute.net>
2025-04-07 18:59:28 +0100

    Add logic to display first answers and remove obsolete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,30 @@ function startTheQuizz() {
     let whatDoYouKnow = document.getElementById('intro-question');
     container.removeChild(whatDoYouKnow);
 
-
+//Shows first question
     let questionElement = document.getElementById("question"); 
     questionElement.textContent = questions[currentQuestion].question;
+
+//In a different container because of styling
+
+    //ButtonA
+    let containerAnswer = document.getElementById('answers');
+    let buttonA = document.createElement('button');
+    buttonA.textContent = questions[currentQuestion].answer;
+    buttonA.id = "answer-a";
+    buttonA.setAttribute("data-type", "AnswerA");
+    containerAnswer.appendChild(buttonA);
+
+    //ButtonB
+    let buttonB = document.createElement('button');
+    buttonB.textContent = questions[currentQuestion].answer;
+    buttonB.id = "answer-b";
+    buttonA.setAttribute("data-type", "AnswerB");
+    containerAnswer.appendChild(buttonB);
+
+    //Removes Start The quizz button
+    let startButton = document.getElementById('start-game');
+    containerAnswer.removeChild(startButton);
 }
+
+
